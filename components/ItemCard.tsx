9e2b6a100af5c5ac9d788a1ee3cea9fa05499200
Eliott Nys <eliott.nys@gmail.com>
2024-06-07 18:48:19 +0200

    added image to itemcard

--- components/ItemCard.tsx
+++ components/ItemCard.tsx
@@ -4,27 +4,27 @@ import { Card, H2, Paragraph, Image, XStack } from "tamagui";
 type ItemCardProps = {
   title: string;
   price: number;
+  imgSrc: string;
 };
 
-const ItemCard: React.FC<ItemCardProps> = ({ title, price }) => {
+const ItemCard: React.FC<ItemCardProps> = ({ title, price, imgSrc }) => {
   return (
     <Card elevate size="$4" bordered>
       <Card.Header padded>
         <H2>{title}</H2>
         <Paragraph theme="alt2">${price.toFixed(2)}</Paragraph>
-      </Card.Header>
-      <Card.Footer></Card.Footer>
-      <Card.Background>
         <Image
           resizeMode="contain"
           alignSelf="center"
           source={{
-            uri: "https://via.placeholder.com/300",
+            uri: "http://192.168.1.20:3000" + imgSrc,
             width: 300,
             height: 300,
           }}
         />
-      </Card.Background>
+      </Card.Header>
+      <Card.Footer></Card.Footer>
+      <Card.Background></Card.Background>
       {/* <Card.Footer padded>
         <Paragraph>{description}</Paragraph>
         <Paragraph>{condition}</Paragraph>
